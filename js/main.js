94e668aa67bff063a1466376ec89aa19f692e895
Woodsii <beanmanwoods@gmail.com>
2024-12-29 18:58:28 -0600

    Debug clientside js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ var lastfmData = {
       dataType: "json",
 
       success: function(resp) {
-        const currPlaying = resp.recenttracks.track[0];
+        const currPlaying = resp.recenttracks.track["0"];
 
         console.log(currPlaying);
         console.log(currPlaying.artist['#text'] + ' HUH fuck');
